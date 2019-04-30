612b40a007ce870fa52f1bc2b7d8715f579120f7
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-04-30 14:34:00 -0400

    fixed css

--- src/User/UserInput.js
+++ src/User/UserInput.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const userInput = props => {
+  const inputStyle = {
+    border: "2px solid red"
+  };
   return (
     <div className="UserInput">
-      <input type="text" onChange={props.changed} />
+      <input type="text" style={inputStyle} onChange={props.changed} />
     </div>
   );
 };
